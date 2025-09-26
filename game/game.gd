6813d2461f4fe7cc95cedfb9fd62775a04380d69
a22Dv <a22dev.gl@gmail.com>
2025-09-26 14:58:20 +0800

    Added custom Queue.

--- game/game.gd
+++ game/game.gd
@@ -3,5 +3,16 @@ extends Node2D
 func _on_button_button_down() -> void:
 	GlobalManager.pop_scene()
 
-func _ready() -> void:
-	pass
+func _ready():
+	var q: Queue = Queue.new()
+	for i in range(5):
+		q.enqueue(i)
+	for i in range(3):
+		q.dequeue()
+	for i in range(6):
+		q.enqueue(i)
+	while not q.empty():
+		print("%d" % [q.dequeue()])
+	
+		
+		
