76947dc9f1d7b5a7fe9af31958a8d3442ed34d7a
a22Dv <a22dev.gl@gmail.com>
2025-09-27 22:28:51 +0800

    Main menu draft.

--- ui/main_menu/main_menu.gd
+++ ui/main_menu/main_menu.gd
@@ -1,27 +1,39 @@
 extends Node2D
 
-@onready var start_btn: Button = $Placeholder/VBoxContainer/Start
-@onready var quit_btn: Button = $Placeholder/VBoxContainer/Quit
-@onready var settings_btn: Button = $Placeholder/VBoxContainer/Settings
+@onready var newgame_btn: Button = $Control/Panel/HBoxContainer/VBoxContainer/NewGame
+@onready var continue_btn: Button = $Control/Panel/HBoxContainer/VBoxContainer/Continue
+@onready var settings_btn: Button = $Control/Panel/HBoxContainer/VBoxContainer/Settings
+@onready var quit_btn: Button = $Control/Panel/HBoxContainer/VBoxContainer/Quit
 
 func _ready() -> void:
+	
+	# Registers itself with the global manager.
+	# This is a one-time requirement due to the fact that 
+	# the main menu is the first scene, which is loaded by Godot and not
+	# by the manager.
 	GlobalManager._register(self)
 	
-	# Call-deferred so that the tree is modified only
-	# after Godot's runtime has finished setting up.
+	var callables: Array[Callable] = [on_newgame, on_continue, on_settings, on_quit]
+	var i: int = 0
+	for btn in [newgame_btn, continue_btn, settings_btn, quit_btn]:
+		btn.button_down.connect(callables[i])
+		i += 1
 	AudioManager.play(
-		AudioManager.AudioEntry.MINUS_DREAMY, 
+		AudioManager.AudioEntry.WHOLESOME_FANTASY,
 		AudioManager.AudioPlaybackType.FADE_IN_OUT_LOOP,
-		false,
-		false,
-		3.0
+		true,
+		true,
+		2.0
 	)
 
-func _on_quit_button_down() -> void:
-	GlobalManager.quit()
-
-func _on_start_button_down() -> void:
+# Currently, new game and continue redirects to a new instance.
+# saving logic required for on_continue() to be functional.
+func on_newgame() -> void:
 	GlobalManager.push_scene("res://game/game.tscn")
-
-func _on_settings_button_down() -> void:
+func on_continue() -> void:
+	GlobalManager.push_scene("res://game/game.tscn")
+func on_settings() -> void:
 	GlobalManager.push_scene("res://ui/settings.tscn")
+func on_quit() -> void:
+	GlobalManager.quit()
+	
