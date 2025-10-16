daf510a0a0b41234ed580f24af55f29284f58373
izeostia20-ai <ize0206@gmail.com>
2025-10-17 01:03:36 +0800

    typing actually works alongside movement and collision nodes

--- miscellaneous/assets/Platformer/character/platform_movement.gd
+++ miscellaneous/assets/Platformer/character/platform_movement.gd
@@ -7,12 +7,13 @@ const DASH_speed = 67
 const DASH_duration = 2
 
 func _physics_process(delta: float) -> void:
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("move_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
